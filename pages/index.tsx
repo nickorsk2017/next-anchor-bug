12cfacc02a3ce63b986c8832f5f9b74d23cf329b
Nick Stepanov <nickorsk2017@gmail.com>
2022-09-28 16:01:39 +0500

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.test}>
+        Test block
+      </div>
+      <div id="apps" className={styles.anchor}>
+        Anchor block
+      </div>
+    </div>
+  );
+}
+
+export default Home
